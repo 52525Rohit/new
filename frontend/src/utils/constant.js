73246893169b0fb97e09484a8147076d711933ba
52525Rohit <147018617+52525Rohit@users.noreply.github.com>
2025-01-11 05:54:06 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const API_END_POINT = "http://localhost:5000/api/v1/user";
+export const API_END_POINT = "https://web-backend-kzv6.onrender.com";
 
 export const options = {
   method: "GET",
